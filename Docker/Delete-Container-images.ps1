071c953372bbb1504e2a9ba8bf5292145c9a960b
Mishra <anup-kumar.mishra@capgemini.com>
2019-12-26 19:08:52 +0530

    Added Docker Scripts

---
+++ Docker/Delete-Container-images.ps1
@@ -0,0 +1,49 @@
+﻿Function Remove-DockerContainers
+{
+    # save containers ids into array (--quiet mean return just containers ids)
+    $containersIds = (docker ps --all --quiet); 
+    if ($containersIds.Count -gt 0)
+    {
+        Write-Host ("removing docker containers: ")
+        # stop all containers
+        docker stop $containersIds
+        # remove all containers
+        docker rm $containersIds
+    }
+}
+Function Remove-DockerImage
+{
+    Param (
+        [Parameter(Mandatory=$True)]
+        [string]$DockerImage
+    )
+    # try find image(s) by name
+    $selectedDockerImages = (docker images --all $DockerImage --quiet)
+    if ($selectedDockerImages.Count -gt 0) 
+    {
+        # remove image(s)
+        docker rmi $DockerImage
+        Write-Host ($DockerImage + " image was removed") -ForegroundColor Green
+    } else {
+        # show mesage that docker image does not exist
+        Write-Host ($DockerImage + " image not exists")  -ForegroundColor Red
+    }
+} 
+Function Remove-DockerImages
+{
+    Param (
+        [Parameter(Mandatory=$True)]
+        [string[]]$DockerImages
+    )
+    Write-Host ("removing docker images: ")
+    foreach ($DockerImage in $DockerImages)
+    {
+        Remove-DockerImage -DockerImage $DockerImage
+    }
+}
+# Array of docker images we want to remove
+$dockerImagesToRemove = @(Read-Host "Enter the Images Name to delete")
+Remove-DockerContainers
+Remove-DockerImages -DockerImages $dockerImagesToRemove
+
+
---
+++ Docker/Delete-Container.ps1
@@ -0,0 +1,20 @@
+﻿$disk = Get-WmiObject Win32_LogicalDisk -ComputerName '.' -Filter "DeviceID='S:'" | Select-Object Size, FreeSpace
+$totalSpace = ([math]::Round($disk.Size / 1GB, 2))
+$freeSpace = ([math]::Round($disk.FreeSpace / 1GB, 2))
+Write-Host "Size of S drive" $totalSpace GB
+Write-Host "Free space on S drive" $freeSpace GB  
+
+# To delete whole images in one run use below code 
+$selectedDockerImages = (docker images --all --quiet)
+foreach($imagename in $selectedDockerImages)
+{
+docker rmi $imagename --force
+}
+
+# Stopping a Container and removing a container with his name. 
+
+$Dockername = Read-Host 'Enter the Conatiner name'
+
+$dockerID = docker inspect --format="{{.ID}}" $Dockername
+docker stop $dockerID
+docker rm $dockerID
